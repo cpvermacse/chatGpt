44bbd76de9757702776e506e35f6a3ed50bea6b0
cpvermacse <cpvermacse@gmail.com>
2023-06-15 16:05:42 +0530

    Create form-tsx3.tsx

---
+++ form-tsx3.tsx
@@ -0,0 +1,127 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@mui/styles';
+import {
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+} from '@mui/material';
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: theme.spacing(2),
+  },
+  submitButton: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const countries = [
+  { code: 'us', name: 'United States' },
+  { code: 'ca', name: 'Canada' },
+  { code: 'uk', name: 'United Kingdom' },
+  // Add more countries as needed
+];
+
+const prefixes = [
+  { code: '+1', name: 'USA (+1)' },
+  { code: '+44', name: 'UK (+44)' },
+  // Add more prefixes as needed
+];
+
+const RegistrationForm = () => {
+  const classes = useStyles();
+  const [country, setCountry] = useState('');
+  const [prefix, setPrefix] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [mobile, setMobile] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleCountryChange = (event) => {
+    setCountry(event.target.value);
+  };
+
+  const handlePrefixChange = (event) => {
+    setPrefix(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform form submission or validation logic here
+  };
+
+  return (
+    <form className={classes.form} onSubmit={handleSubmit}>
+      <FormControl>
+        <InputLabel id="country-label">Country</InputLabel>
+        <Select
+          labelId="country-label"
+          value={country}
+          onChange={handleCountryChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {countries.map((country) => (
+            <MenuItem key={country.code} value={country.code}>
+              {country.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <TextField
+        label="First Name"
+        value={firstName}
+        onChange={(event) => setFirstName(event.target.value)}
+      />
+      <TextField
+        label="Last Name"
+        value={lastName}
+        onChange={(event) => setLastName(event.target.value)}
+      />
+      <FormControl>
+        <InputLabel id="prefix-label">Prefix</InputLabel>
+        <Select
+          labelId="prefix-label"
+          value={prefix}
+          onChange={handlePrefixChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {prefixes.map((prefix) => (
+            <MenuItem key={prefix.code} value={prefix.code}>
+              {prefix.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <TextField
+        label="Mobile"
+        value={mobile}
+        onChange={(event) => setMobile(event.target.value)}
+      />
+      <TextField
+        label="Email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <Button
+        className={classes.submitButton}
+        variant="contained"
+        color="primary"
+        type="submit"
+      >
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+export default RegistrationForm;
